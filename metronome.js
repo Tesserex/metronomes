8dc17b3fd486b1a4754f3c037e7abde50c254e3b
Craig Schwartz <tesserex@gmail.com>
2022-12-28 15:41:26 -0600

    add controls and stuff

--- metronome.js
+++ metronome.js
@@ -1,12 +1,11 @@
-// add a canvas to the body of the html page
-var canvas = document.createElement('canvas');
-document.body.appendChild(canvas);
+var canvas = document.getElementById('canvas');
 
 // get the context and set the width and height
 var ctx = canvas.getContext('2d');
-canvas.width = 500;
-canvas.height = 500;
+canvas.width = canvas.clientWidth;
+canvas.height = canvas.width;
 
+// create an audio context and lower the volume
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var gainNode = audioCtx.createGain();
 gainNode.gain.value = 0.1;
@@ -23,26 +22,71 @@ function playNote(frequency, duration) {
 	oscillator.stop(audioCtx.currentTime + duration);
 }
 
-var bounds = { x: canvas.width / 2, y: canvas.height / 2, radius: canvas.width / 2 };
+// grab all the form inputs
+var ballCount = document.getElementById('ball-count');
+var ballCountLabel = document.getElementById('ball-count-label');
+var pitchOffset = document.getElementById('pitch-offset');
+var pitchOffsetLabel = document.getElementById('pitch-offset-label');
+var showLines = document.getElementById('chkShowLines');
+var simSpeed = document.getElementById('sim-speed');
+var simSpeedLabel = document.getElementById('sim-speed-label');
+var simProgressBar = document.getElementById('sim-progress');
 
-var pitchCents = 50;
-var ballCount = 64;
-var balls = [];
-for (var i = 0; i < ballCount; i++) {
-  balls.push({
-    x: bounds.x,
-    y: bounds.y,
-    speed: 500 * i / ballCount,
-    theta: Math.PI * 2 * i / ballCount,
-    pitch: 110 * Math.pow(2, i * pitchCents / 1200),
-  });
+function updateSimSpeedLabel() {
+  simSpeedLabel.innerHTML = `${simSpeed.value * 100}%`;
 }
 
-var lastTime = 0;
+simSpeed.addEventListener('input', updateSimSpeedLabel);
+updateSimSpeedLabel();
+
+ballCount.addEventListener('input', () => ballCountLabel.innerHTML = ballCount.value);
+pitchOffset.addEventListener('input', () => pitchOffsetLabel.innerHTML = pitchOffset.value);
+
+var progress = 0;
+
+function setProgress(val) {
+  progress = val;
+  if (progress > 1) {
+    progress -= 1;
+  }
+  if (progress < 0) {
+    progress += 1;
+  }
+  simProgressBar.style.width = `${progress*100}%`;
+}
+
+function addProgress(val) {
+  setProgress(progress + val);
+}
+
+var bounds = { x: canvas.width / 2, y: canvas.height / 2, radius: canvas.width / 2 };
+
+var balls = [];
+var loopTime = 1;
+
+var lastTimeMs = 0;
+var running = false;
+
+function initialize() {
+  balls = [];
+  for (var i = 0; i < ballCount.value; i++) {
+    balls.push({
+      x: bounds.x,
+      y: bounds.y,
+      speed: 500 * (i+1) / ballCount.value,
+      theta: Math.PI * 2 * i / ballCount.value,
+    });
+  }
+
+  loopTime = bounds.radius * 2 / balls[0].speed;
+  setProgress(0);
+}
 
 function draw(timestamp) {
-  var dt = timestamp - lastTime;
-  lastTime = timestamp;
+  var dt = (timestamp - lastTimeMs) * simSpeed.value / 1000;
+  lastTimeMs = timestamp;
+
+  addProgress(dt / loopTime);
 
   // fill the background white
   ctx.fillStyle = 'white';
@@ -68,17 +112,17 @@ function draw(timestamp) {
     ctx.stroke();
 
     // draw a black line between this ball and the previous
-    // if (i > 0) {
-    //   ctx.beginPath();
-    //   ctx.moveTo(balls[i - 1].x, balls[i - 1].y);
-    //   ctx.lineTo(ball.x, ball.y);
-    //   ctx.lineWidth = 1;
-    //   ctx.strokeStyle = '#000000';
-    //   ctx.stroke();
-    // }
+    if (i > 0 && chkShowLines.checked) {
+      ctx.beginPath();
+      ctx.moveTo(balls[i - 1].x, balls[i - 1].y);
+      ctx.lineTo(ball.x, ball.y);
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = '#000000';
+      ctx.stroke();
+    }
 
-    ball.x += ball.speed * Math.cos(ball.theta) * dt / 1000;
-    ball.y += ball.speed * Math.sin(ball.theta) * dt / 1000;
+    ball.x += ball.speed * Math.cos(ball.theta) * dt;
+    ball.y += ball.speed * Math.sin(ball.theta) * dt;
 
     // bounce the ball off the bounding circle
     var dx = ball.x - bounds.x;
@@ -97,19 +141,30 @@ function draw(timestamp) {
       ball.x += correctionDistance * Math.cos(ball.theta);
       ball.y += correctionDistance * Math.sin(ball.theta);
       
-      playNote(ball.pitch, 0.1);
+      playNote(110 * Math.pow(2, i * pitchOffset.value / 1200), 0.1);
     }
   }
 
-  requestAnimationFrame(draw);
+  if (running) {
+    requestAnimationFrame(draw);
+  }
 }
 
-// add a button to the page
-var button = document.createElement('button');
-button.innerHTML = 'Start';
-document.body.appendChild(button);
-// when the button is clicked, play a note
-button.addEventListener('click', function() {
-  lastTime = performance.now();
-  requestAnimationFrame(draw);
+const startBtn = document.getElementById('btn-start');
+startBtn.addEventListener('click', function() {
+  if (running) {
+    running = false;
+    startBtn.innerHTML = "Start";
+  } else {
+    lastTimeMs = performance.now();
+    running = true;
+    requestAnimationFrame(draw);
+    startBtn.innerHTML = "Pause";
+  }
 });
+
+document.getElementById('btn-reset').addEventListener('click', function() {
+  initialize();
+});
+
+initialize();
